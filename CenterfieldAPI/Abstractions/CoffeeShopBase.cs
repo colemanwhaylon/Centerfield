aa7b779b2c0aee4f77ceb642c8ca3cad796e8dde
Whaylon Coleman <colemanwhaylon@yahoo.com>
2025-03-10 00:06:53 -0700

    Added a test project and CoffeeShopBase.

---
+++ CenterfieldAPI/Abstractions/CoffeeShopBase.cs
@@ -0,0 +1,17 @@
+﻿namespace CenterfieldAPI.Abstractions
+{
+    public abstract class CoffeeShopBase
+    {
+        public abstract string Name { get; set; }
+        public abstract TimeOnly OpeningTime { get; set; }
+        public abstract TimeOnly ClosingTime { get; set; }
+
+    }
+
+    public class B : CoffeeShopBase
+    {
+        public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override TimeOnly OpeningTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override TimeOnly ClosingTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
